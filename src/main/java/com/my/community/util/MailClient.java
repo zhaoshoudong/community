956b9c39d7f20ec2d3cdaf7f9a3366e3e0010140
zhaosd <2509434424@qq.com>
2022-04-17 19:22:28 +0800

    1、添加发送邮件(HTML)功能及测试方法 2、添加邮件log

---
+++ src/main/java/com/my/community/util/MailClient.java
@@ -0,0 +1,37 @@
+package com.my.community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailClient {
+    public static final Logger log = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender sender;
+    @Value("${spring.mail.username}")
+    String from;
+
+    public void sendMail(String to, String subject, String content) {
+        try {
+            MimeMessage mimeMessage = sender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            sender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            log.error("发送失败!" + e.getMessage());
+        }
+    }
+
+}
---
+++ src/test/java/com/my/community/MailClientTest.java
@@ -0,0 +1,35 @@
+package com.my.community;
+
+import com.my.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailClientTest {
+    @Autowired
+    MailClient mailClient;
+    @Autowired
+    TemplateEngine templateEngine;
+    //发送邮件
+    @Test
+    public void testMail(){
+        mailClient.sendMail("2509434424@qq.com","TestMail","try hard!");
+    }
+    //利用 TemplateEngine 生成Html;发送HTML邮件
+    @Test
+    public void testHtml(){
+        Context context = new Context();
+        context.setVariable("username","Sunday");
+        String process = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail("2509434424@qq.com","HTML",context.toString());
+        System.out.println(process);
+    }
+}
