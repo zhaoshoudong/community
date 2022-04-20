7d6d20e78addfde70899d2a3dbd70efdc911296f
zhaosd <2509434424@qq.com>
2022-04-20 22:58:02 +0800

    1、通过配置注解,在拦截其中拦截所有带有LoginRequired注解方法的请求 2、在需要被拦截的controller方法上添加自定义的LoginRequired注解

---
+++ src/main/java/com/my/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,33 @@
+package com.my.community.controller.interceptor;
+
+import com.my.community.annotation.LoginRequired;
+import com.my.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor {
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+            LoginRequired annotation = method.getAnnotation(LoginRequired.class);
+            if (annotation != null && hostHolder.getUser() == null) {
+                String contextPath = request.getContextPath();
+                response.sendRedirect(contextPath + "/login");
+                return false;
+            }
+        }
+        return true;
+    }
+}
